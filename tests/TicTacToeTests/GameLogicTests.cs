a22e774dccb6357578765989b37c3f5ffb76ae0a
Andrew Lethcoe <alethcoe@gmail.com>
2021-02-09 21:30:53 -0500

    refactoring for tests

---
+++ tests/TicTacToeTests/GameLogicTests.cs
@@ -0,0 +1,39 @@
+using System;
+using Xunit;
+
+namespace TicTacToeTests
+{
+    public class GameLogicTests
+    {
+        [Fact]
+        public void PlayerWins_Row()
+        {
+
+        }
+
+        [Fact]
+        public void PlayerWins_Column()
+        {
+
+        }
+
+        [Fact]
+        public void PlayerWins_Diagonal()
+        {
+
+        }
+
+        [Fact]
+        public void PlayerWins_EmptyBoard()
+        {
+
+        }
+
+        [Fact]
+        public void btnNewGame_Click_ResetsGameBoard()
+        {
+
+        }
+
+    }
+}
