9fa6855bc917aa99b8038d170833ebd2723bb2bf
Andrew Lethcoe <alethcoe@gmail.com>
2021-02-10 22:04:14 -0500

    adding GameLogic Win Tests, slight refactor

--- src/TicTacToe/GameLogic.cs
+++ src/TicTacToe/GameLogic.cs
@@ -25,12 +25,17 @@ namespace TicTacToeVideo
 
         public bool PlayerWin()
         {
+           return PlayerWin(Board);
+        }
+
+        public bool PlayerWin(string[,] board)
+        {
             //we want to check the horizontal row
             for (var i = 0; i < 3; i++)
             {
-                if (!String.IsNullOrWhiteSpace(Board[i, 0]))
+                if (!String.IsNullOrWhiteSpace(board[i, 0]))
                 {
-                    if (Board[i, 0] == Board[i, 1] && Board[i, 0] == Board[i, 2])
+                    if (board[i, 0] == board[i, 1] && board[i, 0] == board[i, 2])
                     {
                         return true;
                     }
@@ -40,9 +45,9 @@ namespace TicTacToeVideo
             //we want to the columns
             for (var i = 0; i < 3; i++)
             {
-                if (!String.IsNullOrWhiteSpace(Board[0, i]))
+                if (!String.IsNullOrWhiteSpace(board[0, i]))
                 {
-                    if (Board[0, i] == Board[1, i] && Board[0, i] == Board[2, i])
+                    if (board[0, i] == board[1, i] && board[0, i] == board[2, i])
                     {
                         return true;
                     }
@@ -51,14 +56,14 @@ namespace TicTacToeVideo
 
             //diagonal
 
-            if (!String.IsNullOrWhiteSpace(Board[1, 1]))
+            if (!String.IsNullOrWhiteSpace(board[1, 1]))
             {
-                if (Board[0, 0] == Board[1, 1] && Board[1, 1] == Board[2, 2])
+                if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
                 {
                     return true;
                 }
 
-                if (Board[0, 2] == Board[1, 1] && Board[1,1] == Board[2, 0])
+                if (board[0, 2] == board[1, 1] && board[1,1] == board[2, 0])
                 {
                     return true;
                 }
@@ -67,9 +72,10 @@ namespace TicTacToeVideo
             return false;
         }
 
-        internal void UpdateBoard(Position postion, string value)
+        public void UpdateBoard(Position postion, string value)
         {
             Board[postion.x, postion.y] = value;
         }
+
     }
 }
--- tests/TicTacToeTests/GameLogicTests.cs
+++ tests/TicTacToeTests/GameLogicTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using TicTacToeVideo;
 using Xunit;
 
 namespace TicTacToeTests
@@ -6,34 +8,68 @@ namespace TicTacToeTests
     public class GameLogicTests
     {
         [Fact]
-        public void PlayerWins_Row()
-        {
-
-        }
-
-        [Fact]
-        public void PlayerWins_Column()
-        {
-
-        }
-
-        [Fact]
-        public void PlayerWins_Diagonal()
-        {
-
-        }
-
-        [Fact]
         public void PlayerWins_EmptyBoard()
         {
-
+            var emptyBoard = new String[,] { { "", "", "" }, { "", "", "" }, { "", "", "" } };
+            var sut = new GameLogic();
+            var result = sut.PlayerWin(emptyBoard);
+            Assert.False(result);
         }
+
+
+        public static IEnumerable<object[]> HorizontalData = new List<object[]>{
+           new object[]{ new string[,]{{"d","d","d"}, {"","",""}, {"","",""}} },
+           new object[]{ new string[,]{{"",""," "}, {"b","b","b"}, {"","",""}} },
+           new object[]{ new string[,]{{"",""," "}, {"","",""}, {"c","c","c"}} }
+        };
+        
+        [Theory]
+        [MemberData(nameof(HorizontalData))]
+        public void PlayerWins_Row(string[,] board)
+        {
+            var sut = new GameLogic();
+            var result = sut.PlayerWin(board);
+            Assert.True(result);
+        }
+
+        public static IEnumerable<object[]> ColumnData = new List<object[]>{
+           new object[]{ new string[,]{{"a","",""}, {"a","",""}, {"a","",""}} },
+           new object[]{ new string[,]{{"","b"," "}, {"","b",""}, {"","b",""}} },
+           new object[]{ new string[,]{{"","","c"}, {"","","c"}, {"","","c"}} }
+        };
+
+        [Theory]
+        [MemberData(nameof(HorizontalData))]
+        public void PlayerWins_Column(string[,] board)
+        {
+            var sut = new GameLogic();
+            var result = sut.PlayerWin(board);
+            Assert.True(result);
+        }
+
+        public static IEnumerable<object[]> DiagData = new List<object[]>{
+           new object[]{ new string[,]{{"x","",""}, {"","x",""}, {"a","","x"}} },
+           new object[]{ new string[,]{{"","b","o"}, {"","o",""}, {"o","b",""}} },
+        };
+
+        [Theory]
+        [MemberData(nameof(DiagData))]
+        public void PlayerWins_Diagonal(string[,] board)
+        {
+            var sut = new GameLogic();
+            var result = sut.PlayerWin(board);
+            Assert.True(result);
+        }
+
+
 
         [Fact]
         public void btnNewGame_Click_ResetsGameBoard()
         {
 
         }
+
+        //test set next player
 
     }
 }
